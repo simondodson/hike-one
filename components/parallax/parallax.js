2f84329a6e7b9239746a3c9edb68f85ab108433a
Vincent van Dijck <vincent@voorhoede.nl>
2020-01-28 11:10:39 +0100

    Revert "fix(parallax) duration is not a prop"
    This reverts commit 13d083c3645df5c661ed8454d7c2a6c8d91c0154.

--- components/parallax/parallax.js
+++ components/parallax/parallax.js
@@ -10,21 +10,22 @@ class Parallax extends Component {
 		this.onResize = this.onResize.bind(this);
 		this.getYOffset = this.getYOffset.bind(this);
 		this.setYOffset = this.setYOffset.bind(this);
-		this.setInitialOffset = this.setInitialOffset.bind(this);
+		this.setInitialOffSet = this.setInitialOffSet.bind(this);
 		this.setOffsetOnResize = this.setOffsetOnResize.bind(this);
+		this.resizeTimer = null;
+		this.speed = props.speed ? 1 - parseFloat(props.speed) : -0.3;
+		this.elementOffset = 0;
+		this.duration = props.duration ? parseInt(props.duration) : 0.3;
 
 		this.state = {
 			ticking: false,
-			speed: props.speed ? 1 - parseFloat(props.speed) : -0.3,
-			elementOffset: 0,
-			duration: 0.3,
 		};
 	}
 
 	componentDidMount() {
 		// only add animation when requestAnimationFrame is supported
 		if (typeof window.requestAnimationFrame !== 'undefined') {
-			this.setInitialOffset();
+			this.setInitialOffSet();
 			window.addEventListener('scroll', this.onScroll);
 			window.addEventListener('resize', this.onResize);
 		}
@@ -62,30 +63,27 @@ class Parallax extends Component {
 		this.setState({ ticking: true });
 	}
 
-	setInitialOffset() {
-		const { elementOffset } = this.state;
+	setInitialOffSet() {
+		this.elementOffset = this.getParallaxYOffset();
 
-		this.setState({ elementOffset: this.getParallaxYOffset() });
-		this.element.style = {
-			transform: `translate3d(0px, ${elementOffset}px, 0px)`, // apply offset
-			visibility: 'visible', // show layers only after offset to prevent jumping animations
-		};
+		// apply offset
+		this.element.style.transform = `translate3d(0px, ${this.elementOffset}px, 0px)`;
+		// show layers only after offset to prevent jumping animations
+		this.element.style.visibility = 'visible';
 	}
 
 	setOffsetOnResize() {
-		let resizeTimer = null;
-		clearTimeout(resizeTimer);
-
-		resizeTimer = setTimeout(() => {
+		// add debounce for resize so it fires only add the end of resize
+		clearTimeout(this.resizeTimer);
+		this.resizeTimer = setTimeout(() => {
 			this.initialScrollHeight = null;
-			this.setInitialOffset();
+			this.setInitialOffSet();
 			const YOffSet = this.getYOffset();
 			this.setYOffset(YOffSet, true);
 		}, 250);
 	}
 
 	getYOffset() {
-		const { elementOffset, speed } = this.state;
 		const scrolledHeight = document.body.scrollTop || document.documentElement.scrollTop || 0;
 
 		// only animate element when in view
@@ -100,21 +98,18 @@ class Parallax extends Component {
 		let relativeScroll = scrolledHeight - this.initialScrollHeight;
 
 		// calculate y offset and return it
-		return relativeScroll * speed + elementOffset;
+		return relativeScroll * this.speed + this.elementOffset;
 	}
 
 	setYOffset(yOffset, noAnimation) {
-		const { duration } = this.state;
-
-		noAnimation
+		this.duration === 0 || noAnimation
 			? // don't use tweenlite if animation is instant
 			  (this.element.style.transform = `matrix(1, 0, 0, 1, 0, ${yOffset})`)
 			: // use tweenlite for a smooth parallax effect
-			  TweenLite.to(this.element, duration, { y: yOffset }, { ease: 'Linear.easeNone' });
+			  TweenLite.to(this.element, this.duration, { y: yOffset }, { ease: 'Linear.easeNone' });
 	}
 
 	getParallaxYOffset() {
-		const { speed } = this.state;
 		const elBoundingRect = this.element.getBoundingClientRect();
 		const windowHeight = document.body.clientHeight || document.documentElement.clientHeight || 0;
 		const scrolledHeight = document.body.scrollTop || document.documentElement.scrollTop || 0;
@@ -130,10 +125,10 @@ class Parallax extends Component {
 
 		if (elementTop > bottomScreen) {
 			// element below viewport
-			return -((elementHalf + windowHalf) * speed);
+			return -((elementHalf + windowHalf) * this.speed);
 		} else if (elementBottom < scrolledHeight) {
 			// element above viewport
-			return (elementHalf + windowHalf) * speed;
+			return (elementHalf + windowHalf) * this.speed;
 		} else {
 			// element is partial in view
 			const viewportMiddle = scrolledHeight + windowHalf;
@@ -144,10 +139,10 @@ class Parallax extends Component {
 
 			if (elementFromMiddle > 0) {
 				// element middle under middle of the viewport
-				return -(elementFromMiddle * speed);
+				return -(elementFromMiddle * this.speed);
 			} else {
 				// element middle over middle of the viewport
-				return -elementFromMiddle * speed;
+				return -elementFromMiddle * this.speed;
 			}
 		}
 	}
@@ -170,6 +165,7 @@ class Parallax extends Component {
 }
 
 Parallax.propTypes = {
+	duration: PropTypes.number,
 	speed: PropTypes.number,
 	children: PropTypes.node,
 };
